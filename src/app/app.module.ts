efad8d7d4e7bf0fcc3d1b1f834d38644534a9ae5
Jaime Norato <jn901113jn@gmail.com>
2023-09-08 00:53:16 -0500

    stilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,8 +19,9 @@ import { JobComponent } from './pages/job/job.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/ip-address/ip-address.component.ts
+++ src/app/pages/ip-address/ip-address.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./ip-address.component.scss']
 })
 export class IpAddressComponent {
+  search : string = '';
+  ip: string = 'IP ADDRESS IP ADDRESS';
+  location: string = 'LOCATION LOCATION';
+  timezone: string = 'TIMEZONE TIMEZONE';
+  isp: string = 'ISP ISP ISP ISP';
 
+  searchIp(){
+    this.ip = this.search;
+    console.log(this.search);
+  }
 }
